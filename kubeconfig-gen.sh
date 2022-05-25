f8fdedd77e2ca6c40e6a5a893257f63f3a229477
Tim Matthews <trmatthe@gmail.com>
2022-05-25 13:06:26 +0100

    fixing unquoted env var use highlighted by shellcheck

--- kubeconfig-gen.sh
+++ kubeconfig-gen.sh
@@ -6,7 +6,7 @@ TOKEN=$(cat /run/secrets/kubernetes.io/serviceaccount/token)
 CERT=/run/secrets/kubernetes.io/serviceaccount/ca.crt
 
 kubectl config set-cluster k8s --server=$API --embed-certs=true --certificate-authority=$CERT
-kubectl config set-credentials k8s-sa --token=$TOKEN
+kubectl config set-credentials k8s-sa --token="$TOKEN"
 kubectl config set-context k8s --cluster k8s --user k8s-sa
 kubectl config use-context k8s
 
